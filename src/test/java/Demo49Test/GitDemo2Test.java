17b8666729a2e13709e66516645f3161a6c07c4e
Priyanko <Priyanko@LAPTOP-NITUV308>
2026-06-19 10:21:23 +0530

    commited

---
+++ src/test/java/Demo49Test/GitDemo2Test.java
@@ -0,0 +1,15 @@
+package Demo49Test;
+
+import org.testng.annotations.Test;
+
+public class GitDemo2Test {
+	
+	@Test (groups="regression")
+	public void git2Demo()
+	{
+		
+		System.out.println("Commando");
+		
+	}
+
+}
---
+++ src/test/java/Demo49Test/GitDemoTest.java
@@ -0,0 +1,14 @@
+package Demo49Test;
+
+import org.testng.annotations.Test;
+
+public class GitDemoTest {
+	
+	@Test (groups="smoke")
+	public void gitDemo1Test()
+	{
+		System.out.println("Cocktail2");
+	}
+	
+
+}
